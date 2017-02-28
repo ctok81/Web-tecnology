17303d738e61c36371a39b3872ba11dab333ddae
ctok81 <savin-m-s-a@yandex.ru>
2017-02-28 11:00:28 +0500

    update css

--- zakaz.php
+++ zakaz.php
@@ -21,13 +21,14 @@
 				<tr><th>Наименование услуги</th><th>Цена</th></tr>
 				<tr><td>Починка компьюктера</td><td>100 $</td></tr>
 				<tr><td>Чтобы всё работало</td><td>100 $</td></tr>
-				<tr><td>Настройка вифи</td><td>100 $</td></tr>
+				<tr><td>Настройка вифи</td><td>50 $</td></tr>
 				<tr><td>Убрать полосы с экрана</td><td>100 $</td></tr>
 				<tr><td>Почистить память и ауру</td><td>100 $</td></tr>
 				<tr><td>Помыть машину</td><td>1000 $</td></tr>
 			</table>
 		</div>
-			<form action="handler.php" method="post">
+		<p>
+			<form align = center action="handler.php" method="post">
 			<input type="text" name="phone" placeholder="Телефон">
 			<input list="lis" name="listbase" placeholder = "Выберите проблему">
 			<input type="submit" value="Отправить">
@@ -36,7 +37,7 @@
 				<option>Серьезная проблема</option>
 				<option>Не очень</option>
 			</datalist> 
-		
+		</p>
 		<footer>Компания /*Наше навзание*/</footer>
 	</body>
 </html>
