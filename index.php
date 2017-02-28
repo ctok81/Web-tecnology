0b5e88ee655a982197e6e273ba86fb71ec213c68
ctok81 <savin-m-s-a@yandex.ru>
2017-02-28 09:47:17 +0500

    create-github

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Ремонт_техники /*Название*/</title>
+		<link rel="stylesheet" type="text/css" href="index.css">
+	</head>
+	<body>
+		<header>  	
+			<nav align=center>
+				<img src = "t.jpg" height=100 width=100 align="center"></img>
+				<ul><a href="index.php">Главная страница</a></ul>
+				<ul><a href="contact.php">Контакты</a></ul>
+				<ul><a href="zakaz.php">Заказать услугу</a></ul>
+			</nav>
+		</header>
+		<article>
+			Мы занимаемся ремонтом вашей техники. Наши специалисты имеют сертификаты....
+		</article>
+		<footer>Компания /*Наше навзание*/</footer>
+	</body>
+</html>
---
+++ zakaz.php
@@ -0,0 +1,42 @@
+<html>
+	<head>
+		<title>Оформить заказ</title>
+		<link rel="stylesheet" type="text/css" href="index.css">
+		<link rel="stylesheet" type="text/css" href="zakaz.css">
+	</head>
+
+	<body>
+		<header> 
+			<nav align=center>
+				<img src = "t.jpg" height=100 width=100></img>
+				<ul><a href="index.php">Главная страница</a></ul>
+				<ul><a href="contact.php">Контакты</a></ul>
+				<ul><a href="zakaz.php">Заказать услугу</a></ul>
+			</nav>
+		</header>
+		
+		<div align=center>
+			<p></p>
+			<table align=center>
+				<tr><th>Наименование услуги</th><th>Цена</th></tr>
+				<tr><td>Починка компьюктера</td><td>100 $</td></tr>
+				<tr><td>Чтобы всё работало</td><td>100 $</td></tr>
+				<tr><td>Настройка вифи</td><td>100 $</td></tr>
+				<tr><td>Убрать полосы с экрана</td><td>100 $</td></tr>
+				<tr><td>Почистить память и ауру</td><td>100 $</td></tr>
+				<tr><td>Помыть машину</td><td>1000 $</td></tr>
+			</table>
+		</div>
+			<form action="handler.php" method="post">
+			<input type="text" name="phone" placeholder="Телефон">
+			<input list="lis" name="listbase" placeholder = "Выберите проблему">
+			<input type="submit" value="Отправить">
+		
+			<datalist id="lis">
+				<option>Серьезная проблема</option>
+				<option>Не очень</option>
+			</datalist> 
+		
+		<footer>Компания /*Наше навзание*/</footer>
+	</body>
+</html>
