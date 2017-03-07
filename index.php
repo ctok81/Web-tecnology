81caae4991ef02ca25ab05d3a845a85b43e4500d
ctok81 <savin-m-s-a@yandex.ru>
2017-03-07 10:53:51 +0500

    max

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
 	<head>
-		<title>Ремонт_техники /*Название*/</title>
+		<title>Ремонт техники /*Название*/</title>
 		<link rel="stylesheet" type="text/css" href="index.css">
 	</head>
 	<body>
-		<header>  	
+		<header>
 			<nav align=center>
 				<img src = "t.jpg" height=100 width=100 align="center"></img>
 				<ul><a href="index.php">Главная страница</a></ul>
@@ -12,7 +12,7 @@
 				<ul><a href="zakaz.php">Заказать услугу</a></ul>
 			</nav>
 		</header>
-		
+
 		<article>
 			Мы занимаемся ремонтом вашей техники. Наши специалисты имеют сертификаты....
 		</article>
--- zakaz.php
+++ zakaz.php
@@ -4,7 +4,7 @@
 		<link rel="stylesheet" type="text/css" href="index.css">
 		<link rel="stylesheet" type="text/css" href="zakaz.css">
 		<link rel="stylesheet" type="text/css" href="divtable.css">
-		
+
 <script type="text/javascript">
 function hw(id) {
 	document.location.href="?gri=" + id;
@@ -15,7 +15,7 @@ function hw(id) {
 	</head>
 
 	<body>
-		<header> 
+		<header>
 			<nav align=center>
 				<img src = "t.jpg" height=100 width=100></img>
 				<ul><a href="index.php">Главная страница</a></ul>
@@ -24,7 +24,7 @@ function hw(id) {
 			</nav>
 		</header>
 		<br/>
-		<?php 
+		<?php
 		include("bd.php");
 
 		$sqlGroups = mysql_query("SELECT name FROM Group_Services WHERE 1;",$db);
@@ -34,47 +34,46 @@ function hw(id) {
 		$co=0;
 		$gr_i=1;
 
-		While($tablerows = mysql_fetch_row($sqlGroups))
+		While($grouprows = mysql_fetch_row($sqlGroups))
 			{
 				$co=$co+1;
-			echo("<div class='Li2st' onclick='hw($co); ' >$tablerows[0]</div>");
-			}	
+			echo("<div class='Li2st' onclick='hw($co); ' >$grouprows[0]</div>");
+			}
 		echo"</div>";
-		
-		if(isset($_GET['gri'])&& $_GET['gri']<7)
+
+		if(isset($_GET['gri']) && $_GET['gri']<7)
 		{
 			$gr_i=$_GET['gri'];
 			echo"<div class='divTable' style='width: 60%; border: 1px solid #000;'>";
 		echo"<div class='divTableBody'>";
-		
+
 		$sqlServices = mysql_query("SELECT name,pricer FROM `Service` WHERE group_id=$gr_i",$db);
-		While($tablerows2 = mysql_fetch_row($sqlServices))
+		While($problemrows = mysql_fetch_row($sqlServices))
 			{
 			echo"<div class='divTableRow'>";
-			echo"<div class='divTableCell'>$tablerows2[0]</div>";
-			echo"<div class='divTableCell'>$tablerows2[1]</div>";
+			echo"<div class='divTableCell'>$problemrows[0]</div>";
+			echo"<div class='divTableCell'>$problemrows[1]</div>";
 			echo"</div>";
-			}	
+			}
 		 echo"</div>";
 		echo"</div>";
+	}
+	else
+	{
 
-		}
-		else
-		{
-		
 
-		}
+	}
 ?>
 		<p>
 			<form align = center action="handler.php" method="post">
 			<input type="text" name="phone" placeholder="Телефон">
 			<input list="lis" name="listbase" placeholder = "Выберите проблему">
 			<input type="submit" value="Отправить">
-		
+
 			<datalist id="lis">
 				<option>Серьезная проблема</option>
 				<option>Не очень</option>
-			</datalist> 
+			</datalist>
 		</p>
 		<footer>Компания /*Наше навзание*/</footer>
 	</body>
