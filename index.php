131e900f41595df133a7386c7ff6222e3ffe00b7
ctok81 <savin-m-s-a@yandex.ru>
2017-03-20 22:32:02 +0500

    Сдавай

--- index.php
+++ index.php
@@ -4,12 +4,18 @@
 		<link rel="stylesheet" type="text/css" href="index.css">
 	</head>
 	<body>
+
 	<aside class="maxPars">
 		<?php
-		$python =  shell_exec('C:\Users\Navai\AppData\Local\Programs\Python\Python36-32\python.exe C:\OpenServ\OpenServer\domains\localhost\Web-tecnology\parser.py');
+		$Max_path = 'C:\Python\python.exe C:\OpenServer\domains\localhost\parser.py';
+		$Max_path2 = 'C:\Python\python.exe C:\OpenServer\domains\localhost\gitparser.py';
+		$Roma_path = 'C:\Users\Navai\AppData\Local\Programs\Python\Python36-32\python.exe C:\OpenServ\OpenServer\domains\localhost\Web-tecnology\parser.py';
+		$Roma_path2 = 'C:\Users\Navai\AppData\Local\Programs\Python\Python36-32\python.exe C:\OpenServ\OpenServer\domains\localhost\Web-tecnology\gitparser.py';
+		$python =  shell_exec($Max_path);
 		echo mb_convert_encoding($python, 'UTF-8', 'Windows-1251')
-		?>	
+		?>
 	</aside>
+
 		<header class="menu">
 			<nav align=center>
 				<img src = "t.jpg" height=100 width=100 align="center"></img>
@@ -18,22 +24,15 @@
 				<ul><a class="top" href="zakaz.php">Заказать услугу</a></ul>
 			</nav>
 		</header>
+
 		<section class="news">
 		<header class="news">
 		<?php
-			$pythonR =  shell_exec('C:\Users\Navai\AppData\Local\Programs\Python\Python36-32\python.exe C:\OpenServ\OpenServer\domains\localhost\Web-tecnology\gitparser.py');
+			$pythonR =  shell_exec($Max_path2);
 		echo mb_convert_encoding($pythonR, 'UTF-8', 'Windows-1251')
 		?>
 		</header>
-					<article>
-						Мы занимаемся ремонтом вашей техники. Наши специалисты имеют сертификаты....
-					</article>
-					<article>
-						Мы занимаемся ремонтом вашей техники. Наши специалисты имеют сертификаты....
-					</article>
-					<article>
-						Мы занимаемся ремонтом вашей техники. Наши специалисты имеют сертификаты....
-					</article>
+
 					<article>
 						Мы занимаемся ремонтом вашей техники. Наши специалисты имеют сертификаты....
 					</article>
--- parser.py
+++ parser.py
@@ -5,8 +5,9 @@ import re
 
 f = urllib.request.urlopen('http://yandex.ru/pogoda/perm')
 soup = BeautifulSoup(f, fromEncoding="UTF-8")
+'current-weather__thermometer current-weather__thermometer_type_now'
 pogoda = soup.find('div', {'class':'current-weather__thermometer current-weather__thermometer_type_now'}).getText()
 time = soup.find('span', {'class':'current-weather__local-time'})
 
-print(u'<p><span class="Date1">Текущее время:</span><span class="Value">', time.getText(), '</span><br>')
-print(u'<span class="Date1">Температура в Перми:</span><span class="Value">', re.search(r"[+,-]?\d*",pogoda).group(0) ,'°C</span></p>')
+print(u'<span class="Date1">Текущее время:</span><span class="Value">', time.getText(), '</span><br>')
+print(u'<span class="Date1">Температура:</span><span class="Value">', re.search(r"[\−\-\+]?\d*",pogoda).group(0) ,'°C</span>')
