da12ef864866428e2e14c4989f473ad104210878
ctok81 <savin-m-s-a@yandex.ru>
2017-03-27 19:48:32 +0500

    lab_2_Parsing

--- parser.py
+++ parser.py
@@ -3,11 +3,9 @@ from bs4 import BeautifulSoup
 import urllib.request
 import re
 
-f = urllib.request.urlopen('http://yandex.ru/pogoda/perm')
-soup = BeautifulSoup(f, fromEncoding="UTF-8")
-'current-weather__thermometer current-weather__thermometer_type_now'
+soup = BeautifulSoup(urllib.request.urlopen('http://yandex.ru/pogoda/perm'))
 pogoda = soup.find('div', {'class':'current-weather__thermometer current-weather__thermometer_type_now'}).getText()
 time = soup.find('span', {'class':'current-weather__local-time'})
 
-print(u'<span class="Date1">Текущее время:</span><span class="Value">', time.getText(), '</span><br>')
-print(u'<span class="Date1">Температура:</span><span class="Value">', re.sub('−','-',re.search(r'[\−\-\+]?\d*',pogoda).group(0)) ,'°C</span>')
+print('<span class="Date1">Текущее время:</span><span class="Value">', time.getText(), '</span><br>')
+print('<span class="Date1">Температура:</span><span class="Value">', re.sub('−','-',re.search('[\−\-\+]?\d*',pogoda).group(0)) ,'°C</span>')
