edad45c6ccf27827e0cb55df8cd113c68e50655f
ctok81 <savin-m-s-a@yandex.ru>
2017-06-08 14:46:02 +0500

    3 lab, v.1.1, Валидация на стороне клиента

--- zakaz.php
+++ zakaz.php
@@ -74,26 +74,21 @@ function hw(id) {
 						}
 					echo"</div>";//table-body
 				echo"</div>";//table-cell
-
+				$class_name = 'divTableCell';
 	}
 	else
 	{
 		$flag = false;
+		$class_name ='divTableCellnone';
 	}
-
-			if($flag)
-				echo "<div class='divTableCell'>";
-			if(!$flag)
-				echo "<div class='divTableCellnone'>";
 ?>
+				<div class=$class_name;>
 					<form align="center" action="handler2.php" target="area" method="post" style="border: 5px; margin: 5px; padding: 3px 5px;">
 						<input type='tel' required name="phone" placeholder="Ваш Телефон. Пример: 71234567890" pattern='7[\d]{10}' maxlength="11" title="Пример: 71112345678">
 						<input type="text" name="fio" placeholder="Ваш ФИО">
 						<input type="text" name="chto" placeholder="Что-нибудь">
 						<input type="submit" value="Заказать услугу">
 					</form>
-
-
 				</div>
 			</div>
 
