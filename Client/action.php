5130ee049e6362ba1ae717870d1d525b34c01aef
ctok81 <savin-m-s-a@yandex.ru>
2017-06-08 17:38:34 +0500

    5 lab. v.1.1

--- Client/action.php
+++ Client/action.php
@@ -1,54 +1,35 @@
 <?
-header('Content-Type: text/html; charset=utf-8');
-?>
-
-<html>
-  <head>
-    <title>Заказы</title>
-		  <link rel="stylesheet" type="text/css" href="client.css">
-  </head>
-
-  <body>
-
-      <?php>
-      include("/../bd.php");
-      $phone = $_POST['phone'];
-      $query ="SELECT phone, FIO, chto FROM Client where phone like('$phone')";
-  		$masClient = mysql_query($query,$db);
-
+  include("/../bd.php");
+  $q = $_REQUEST["q"];
+  if(ctype_digit($q))
+  {
+    if(iconv_strlen($q) == 11 & iconv_strlen($q) > 0)
+      $query = "SELECT phone, FIO, chto FROM Client where phone like('$q');";
+    else
+      if(iconv_strlen($q) > 0 & iconv_strlen($q) <= 10)
+        $query = "SELECT phone, FIO, chto FROM Client where phone like('$q%');";
+    $masClient = mysql_query($query,$db);
     if(mysql_num_rows($masClient) > 0)
     {
-      echo "
-        <table>
-        <caption>Заказы</caption>
-        <tr>
-        <th>Телефон</th>
-        <th>ФИО</th>
-        <th>Адрес</th>
-        </tr>";
-        while(list($n, $p, $c) = mysql_fetch_array($masClient)) {
-          echo"<tr>
+      echo "<caption>Заказы</caption>
+            <tr>
+              <th>Телефон</th>
+              <th>ФИО</th>
+              <th>Адрес</th>
+            </tr>";
+      while(list($n, $p, $c) = mysql_fetch_array($masClient))
+      {
+        echo"<tr>
               <td>$n</td>
               <td>$p</td>
               <td>$c</td>
-              </tr>";
-        }
-        echo "</table>";
+            </tr>";
+      }
     }
     else
-    {
-      echo "<p>Не найдены</p>";
-    }
-    ?>
+      echo "С таким телефоном не найдены.";
 
-
-    <form method = "post" action = "search.php">
-      <input type="submit" value="Вернуться к поиску">
-    </form>
-
-<form method="post" action="/../">
-  <input type = "submit" value="Вернуться в главное меню">
-</form>
-
-  </body>
-</html>
+  }
+  else
+    echo "Пожалуйста, вводите цифры";
+?>
--- Client/search.php
+++ Client/search.php
@@ -2,15 +2,43 @@
 header('Content-Type: text/html; charset=utf-8');
 ?>
 
+<script type="text/javascript">
+function startAjax(url)
+{
+  var request;
+  if(window.XMLHttpRequest)
+      request = new XMLHttpRequest();
+  else
+    if(window.ActiveXObject)
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+      else
+        return;
+
+  request.onreadystatechange = function()
+  {
+    if(request.readyState == 4)
+      if(request.status==200)
+        document.getElementById("printResult").innerHTML = request.responseText;
+  }
+  request.open("GET", url + "?q=" + document.getElementsByTagName('input')[0].value, true);
+  request.send();
+}
+</script>
+
 <html>
   <head>
-    <title> Поиск в БД </title>
+    <title>Заказы</title>
+		  <link rel="stylesheet" type="text/css" href="client.css">
   </head>
   <body>
-    <form method = "post" action = "action.php">
-      <include type="text" name="fio">
-      <input type='tel' required name="phone" placeholder="79%" pattern='7[0-9]{1,10}%{0,1}' maxlength="11" title="Пример: 7900%">
-      <input type="submit" value="Отправить">
+    <form>
+      <input type='tel' required name="phone" placeholder="Пример: 79" pattern='7[0-9]{1,10}'
+      maxlength="11" title="Пример: 79" onkeyup="startAjax('action.php');">
     </form>
+    <form method="post" action="/../">
+      <input type = "submit" value="Вернуться в главное меню">
+    </form>
+    <table id="printResult">
+    </table>
   </body>
 </html>
