27de7f514636807f943b0a14ce69409da1a7f62f
ctok81 <savin-m-s-a@yandex.ru>
2017-06-23 13:17:43 +0500

    Лабораторная работа №5 сдана. Савин М.А.

---
+++ search.php
@@ -0,0 +1,50 @@
+<?
+header('Content-Type: text/html; charset=utf-8');
+?>
+
+<script type="text/javascript">
+function startAjax(url)
+{
+  var request;
+  if(window.XMLHttpRequest)
+      request = new XMLHttpRequest();
+  else
+    if(window.ActiveXObject)
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+      else
+        return;
+
+  request.onreadystatechange = function()
+  {
+    if(request.readyState == 4)
+      if(request.status == 200)
+        {
+          document.getElementById("printResult").innerHTML = request.responseText;
+          document.getElementById("test").innerHTML += "Нажата клавиша ";
+        }
+  }
+  request.open("GET", url + "?q=" + document.getElementsByTagName('input')[0].value, true);
+  request.send();
+}
+</script>
+
+<html>
+  <head>
+    <title>Заказы</title>
+		  <link rel="stylesheet" type="text/css" href="client.css">
+  </head>
+  <body>
+    <form>
+      <input type='tel' required name="phone" placeholder="Пример: 79" pattern='7[0-9]{1,10}'
+      maxlength="11" title="Пример: 79" onkeyup="startAjax('action.php');">
+    </form>
+    <form method="post" action="/../">
+      <input type = "submit" value="Вернуться в главное меню">
+    </form>
+    <div id = "test"> test:  </div>
+    <table id="printResult">
+    </table>
+  </body>
+</html>
+
+//сдана
