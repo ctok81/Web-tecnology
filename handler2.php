cfa10630619e2e24ca47406514a86812d4c612ab
KrivoshchekovR <mertvec123@yandex.ru>
2017-11-20 14:18:32 +0500

    4 лаба?
    1

--- handler2.php
+++ handler2.php
@@ -1,16 +1,29 @@
-<meta charset="utf-8">
-<?php
-	$p = stripslashes($_POST['phone']);
-	$p= htmlspecialchars($p);
-	$f = stripslashes($_POST['fio']);
-	$f= htmlspecialchars($f);
-	$date = date('Y-m-d H:i:s',time()+(2*3600));
-	$link = mysqli_connect("127.0.0.1","Admin","963258741", "Web-tech");
-
-	if(mysqli_query($link, "INSERT INTO `Calls` VALUES(NULL, $f, '$p', '$date')") === TRUE)
-	print "<script language='Javascript'>
-			window.location.href = 'http://localhost/Web-tecnology/zakaz.php';
-			alert('Ваш запрос отправлен');
-			 </script>";
-				exit;
+<?
+header('Content-Type: text/html; charset=utf-8');
 ?>
+
+<?php
+
+if((is_numeric($_POST['phone']) & preg_match("/7\d{10}/", $_POST['phone']) == 1)&&(is_string($_POST['fio']) & preg_match("/[А-Яа-ё]{5,}\s[А-Яа-ё]{4,}\s[А-Яа-ё]{5,}/", $_POST['fio']) == 1))
+{
+	//echo ("ok");
+	require_once('/closed/bd.php');
+	$p = htmlspecialchars($_POST['phone']);
+	$f = htmlspecialchars($_POST['fio']);
+	$c = htmlspecialchars($_POST['chto']);
+
+	$link = mysqli_connect($host, $user, $password, $database);																								//подключение
+	$query = sprintf("INSERT INTO `Client` VALUES(default, %s, '%s', '%s')", $p, $f, $c); 										//форматирование строки
+	mysqli_query($link, $query);																																							//выполнение запроса
+	mysqli_close($link);																																											//закрытие соединения
+}
+
+?>
+<script language='Javascript'>
+function reload()
+			{
+				location = "zakaz.php"
+			};
+				setTimeout('reload()', 4000);
+</script>
+<div>Сообщение отправлено! Подождите, сейчас вы будете перенаправлены на главную страницу...</div>
--- zakaz.php
+++ zakaz.php
@@ -84,9 +84,9 @@ function hw(id) {
 	}
 ?>
 				<div class=$class_name;>
-					<form align="center" action="handler.php" target="area" method="post" style="border: 5px; margin: 5px; padding: 3px 5px;">
+					<form align="center" action="handler2.php" target="area" method="post" style="border: 5px; margin: 5px; padding: 3px 5px;">
 						<input type='tel' required name="phone" placeholder="Ваш Телефон. Пример: 71234567890" pattern='7\d{10}' maxlength="11" title="Пример: 71112345678">
-						<input type="text" name="fio" placeholder="Ваш ФИО">
+						<input type="text" name="fio" placeholder="Ваш ФИО" pattern="^([А-Яа-ё]{5,})\s([А-Яа-ё]{4,})\s([А-Яа-ё]{5,})?" title="Пример: Кривощёков Иван Иванович">
 						<input type="text" name="chto" placeholder="Что-нибудь">
 						<input type="submit" value="Заказать услугу">
 					</form>
